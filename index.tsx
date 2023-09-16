fd45cde50335510deef16eb740c13d0a9345e887
vlad <vladlisin2@gmail.com>
2023-09-16 17:20:00 +0700

    test

--- index.tsx
+++ index.tsx
@@ -21,6 +21,23 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { persistStore } from 'redux-persist'
 import store from './src/store'
 import { PersistGate } from 'redux-persist/integration/react'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA24GVDgUvN-bKW0ntxDeAzyhn02rM68mg",
+    authDomain: "project-constructor-aeff5.firebaseapp.com",
+    projectId: "project-constructor-aeff5",
+    storageBucket: "project-constructor-aeff5.appspot.com",
+    messagingSenderId: "134796090145",
+    appId: "1:134796090145:web:8a8087b8c3a08347f7bc60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const persistor = persistStore(store);
 
